9ec271a41898b3f3d575682c5c95c15a77fb6a25
Love dazzell <lovepreetdazzell@gmail.com>
2026-06-15 08:57:55 +0530

    update code

--- components/variable.js
+++ components/variable.js
@@ -1,3 +1,7 @@
-export const apiBaseUrl = 'http://103.223.12.202:9000/user-app/';
-// export const REACT_APP_HOST_API_URL = 'https://api.universalhra.com';
-export const REACT_APP_HOST_API_URL = 'http://103.223.12.202:9000';
+// export const apiBaseUrl = 'http://103.223.12.202:9000/user-app/';
+// export const REACT_APP_HOST_API_URL = 'http://103.223.12.202:9000';
+
+
+// Production
+export const apiBaseUrl = 'https://api.universalhra.com/user-app/';
+export const REACT_APP_HOST_API_URL = 'https://api.universalhra.com';
